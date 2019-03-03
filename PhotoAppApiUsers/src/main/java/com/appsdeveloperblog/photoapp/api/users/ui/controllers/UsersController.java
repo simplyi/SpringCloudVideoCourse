488f909984a003b8abc450699d9c543b238f4797
Sergey Kargopolov <skargopolov@central1.com>
2019-03-02 17:19:57 -0800

    Zuul as load balancer

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,17 +1,23 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/status/check")
 	public String status()
 	{
-		return "Working";
+		return "Working on port " + env.getProperty("local.server.port");
 	}
 
 }
