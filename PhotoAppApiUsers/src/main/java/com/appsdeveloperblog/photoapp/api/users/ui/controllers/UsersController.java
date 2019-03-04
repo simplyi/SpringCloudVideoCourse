5f7c85591611229911ff4850a307066bf9e32c3e
Sergey Kargopolov <skargopolov@central1.com>
2019-03-04 09:23:23 -0800

    Added CreateUserRequestModel and Validation of RequestBody

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,11 +1,14 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -19,5 +22,11 @@ public class UsersController {
 	{
 		return "Working on port " + env.getProperty("local.server.port");
 	}
-
+ 
+	
+	@PostMapping
+	public String createUser(@RequestBody CreateUserRequestModel userDetails)
+	{
+		return "Create user method is called";
+	}
 }
---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/model/CreateUserRequestModel.java
@@ -0,0 +1,51 @@
+package com.appsdeveloperblog.photoapp.api.users.ui.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreateUserRequestModel {
+	
+	@NotNull(message="First name cannot be null")
+	@Size(min=2, message= "First name must not be less than two characters")
+	private String firstName;
+	
+	@NotNull(message="Last name cannot be null")
+	@Size(min=2, message= "Last name must not be less than two characters")
+	private String lastName;
+	
+	@NotNull(message="Password cannot be null")
+	@Size(min=8, max=16, message="Password must be equal or grater than 8 characters and less than 16 characters")
+	private String password;
+	
+	@NotNull(message="Email cannot be null")
+	@Email
+	private String email;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
