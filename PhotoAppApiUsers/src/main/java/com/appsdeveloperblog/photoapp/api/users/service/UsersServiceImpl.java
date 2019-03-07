ddac178f5e827704c4c173253ee9f41869a9f7b3
Sergey Kargopolov <skargopolov@central1.com>
2019-03-07 13:48:57 -0800

    Storing UserEntity to database

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -2,17 +2,39 @@ package com.appsdeveloperblog.photoapp.api.users.service;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import com.appsdeveloperblog.photoapp.api.users.data.*;
 
+@Service
 public class UsersServiceImpl implements UsersService {
-
+	
+	UsersRepository usersRepository;
+	
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository)
+	{
+		this.usersRepository = usersRepository;
+	}
+ 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		// TODO Auto-generated method stub
 		
 		userDetails.setUserId(UUID.randomUUID().toString());
+		ModelMapper modelMapper = new ModelMapper(); 
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+		userEntity.setEncryptedPassword("test");
 		
 		
+		usersRepository.save(userEntity);
+ 
 		return null;
 	}
 
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
- import org.springframework.beans.factory.annotation.Autowired;
+ import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
 
+import com.appsdeveloperblog.photoapp.api.users.service.*;
+import com.appsdeveloperblog.photoapp.api.users.shared.*;
+
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	UsersService usersService;
 
 	@GetMapping("/status/check")
 	public String status()
@@ -27,6 +36,12 @@ public class UsersController {
 	@PostMapping
 	public String createUser(@RequestBody CreateUserRequestModel userDetails)
 	{
+		ModelMapper modelMapper = new ModelMapper(); 
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+		usersService.createUser(userDto);
+		
 		return "Create user method is called";
 	}
 }
