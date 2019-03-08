9e3a131f980457324b601a4d249b6034f4ab54a6
Sergey Kargopolov <skargopolov@central1.com>
2019-03-08 07:33:55 -0800

    Added WebSecurity to Users Microservice

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -0,0 +1,18 @@
+package com.appsdeveloperblog.photoapp.api.users.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/users/**").permitAll();
+	}
+
+}
