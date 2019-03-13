38698b353b534221b37f6b3083d3f151644e4f57
Sergey Kargopolov <skargopolov@central1.com>
2019-03-13 06:49:03 -0700

    Implemented LoginRequestModel

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/model/LoginRequestModel.java
@@ -0,0 +1,23 @@
+package com.appsdeveloperblog.photoapp.api.users.ui.model;
+
+public class LoginRequestModel {
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
