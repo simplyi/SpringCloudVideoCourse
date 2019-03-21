ed05a831ad89de38bbfa3f4d1a7c43a60b4a68ea
Sergey Kargopolov <skargopolov@central1.com>
2019-03-21 06:13:27 -0700

    Removing ignored files

--- PhotoAppApiZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/security/WebSecurity.java
+++ PhotoAppApiZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.photoapp.api.gateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     	http.csrf().disable(); 
     	http.headers().frameOptions().disable();
+    	http.authorizeRequests()
+    	.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
+    	.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+    	.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+    	.anyRequest().authenticated();
     	
     	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     	
