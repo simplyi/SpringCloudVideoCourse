7f3474baae6669c656a6f0626c4ebbb8e9b0fc29
Sergey Kargopolov <skargopolov@central1.com>
2019-04-08 06:20:03 -0700

    Added RabbitMQ Server default connection credentials

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -32,7 +32,7 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status()
 	{
-		return "Working on port " + env.getProperty("local.server.port");
+		return "Working on port " + env.getProperty("local.server.port") + ", with token = " + env.getProperty("token.secret");
 	}
  
 	@PostMapping(
