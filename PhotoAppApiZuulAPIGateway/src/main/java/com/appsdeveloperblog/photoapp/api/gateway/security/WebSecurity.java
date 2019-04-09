c0ba3fdda2637f4892d528933114e1c0f7d4c29c
Sergey Kargopolov <skargopolov@central1.com>
2019-04-09 09:15:01 -0700

    Enabled Spring Boot Actuator Endpoints on Zuul API Gateway

--- PhotoAppApiZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/security/WebSecurity.java
+++ PhotoAppApiZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/security/WebSecurity.java
@@ -26,6 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     	http.csrf().disable(); 
     	http.headers().frameOptions().disable();
     	http.authorizeRequests()
+    	.antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
     	.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
     	.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
     	.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
