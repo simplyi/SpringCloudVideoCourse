c3fea11203ba9ef1f0e8017519e2faa9d8f3f499
Sergey Kargopolov <skargopolov@central1.com>
2019-05-19 04:26:34 -0700

    Added Hystrix circuit breaker support and a callback method to Feign client

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -16,6 +17,7 @@ import feign.Logger;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -1,16 +1,29 @@
 package com.appsdeveloperblog.photoapp.api.users.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
 	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient
+{
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		// TODO Auto-generated method stub
+		return new ArrayList<>();
+	}
 }
