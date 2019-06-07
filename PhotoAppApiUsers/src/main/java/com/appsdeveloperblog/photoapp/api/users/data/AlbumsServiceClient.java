1c52582b39ea1d234a6c8fc9d57117b5513486a8
Sergey Kargopolov <skargopolov@central1.com>
2019-06-07 07:20:35 -0700

    Added Distributed tracing support with Sleuth and Zipkin

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -3,6 +3,8 @@ package com.appsdeveloperblog.photoapp.api.users.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 
 	@GetMapping("/users/{id}/albums")
@@ -18,12 +23,38 @@ public interface AlbumsServiceClient {
 }
 
 @Component
-class AlbumsFallback implements AlbumsServiceClient
-{
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+
+	@Override
+	public AlbumsServiceClient create(Throwable cause) {
+		// TODO Auto-generated method stub
+		return new AlbumsServiceClientFallback(cause);
+	}
+
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private final Throwable cause;
+
+	public AlbumsServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
 
 	@Override
 	public List<AlbumResponseModel> getAlbums(String id) {
 		// TODO Auto-generated method stub
+
+		if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getAlbums was called with userId: " + id + ". Error message: "
+					+ cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: " + cause.getLocalizedMessage());
+		}
+
 		return new ArrayList<>();
 	}
+
 }
