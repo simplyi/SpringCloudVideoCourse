2424c86e0eeee5a6f84a20037d75621db9d0b3cf
Sergey Kargopolov <skargopolov@central1.com>
2019-08-25 07:02:07 -0700

    Enabled Security for Eureka Discovery Dashboard

---
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -0,0 +1,21 @@
+package com.appsdeveloperblog.photoapp.discovery;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
