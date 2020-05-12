22c6324e3fa229fba8ba346f297271d112aedb3c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-05-12 13:34:37 -0400

    Spring Boot Profiles

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.photoapp.api.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,6 +23,9 @@ import feign.Logger;
 @EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
+	@Autowired
+	Environment environment;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
 	}
@@ -38,10 +44,40 @@ public class PhotoAppApiUsersApplication {
 	}
 	
 	@Bean
+	@Profile("production")
 	Logger.Level feignLoggerLevel()
+	{
+		return Logger.Level.NONE;
+	}
+	
+	@Bean
+	@Profile("!production")
+	Logger.Level feignDefaultLoggerLevel()
 	{
 		return Logger.Level.FULL;
 	}
+	
+	@Bean
+	@Profile("production")
+	public String createProductionBean() {
+		System.out.println("Production bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
+		return "Production bean";
+	}
+	
+	@Bean
+	@Profile("!production")
+	public String createNotProductionBean() {
+		System.out.println("Not Production bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
+		return "Not production bean";
+	}
+	
+	@Bean
+	@Profile("default")
+	public String createDevelopmentBean() {
+		System.out.println("Development bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
+		return "Development bean";
+	}
+	
 	/*
 	@Bean
 	public FeignErrorDecoder getFeignErrorDecoder()
