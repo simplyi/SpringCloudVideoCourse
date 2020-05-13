7a3cbae52aee6ca1e2f7f1ea7cf970278d28d769
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-05-13 13:46:28 -0400

    Register Authorization Filter in Users Microservices and configure antMatchers.

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.photoapp.api.users.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,9 +32,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/users").hasIpAddress(environment.getProperty("gateway.ip"))
+		.antMatchers("/h2-console/**").permitAll()
+		.anyRequest().authenticated()
 		.and()
-		.addFilter(getAuthenticationFilter());
+		.addFilter(getAuthenticationFilter())
+		.addFilter(new AuthorizationFilter(authenticationManager(), environment));
 		http.headers().frameOptions().disable();
 	}
 	
