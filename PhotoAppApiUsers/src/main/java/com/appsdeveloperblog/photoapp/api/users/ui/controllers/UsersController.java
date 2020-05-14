91ad51b4ee0b5466784e47299e9be9ede4dc06d2
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-05-14 18:06:57 -0400

    Added the @PreAuthorize annotation to UsersController
    (cherry picked from commit 27dc4ef30927068be1d6bc23dac70b452477e52b)

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,7 @@ public class UsersController {
 	}
 	
     @GetMapping(value="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PreAuthorize("principal == #userId")
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
        
         UserDto userDto = usersService.getUserByUserId(userId); 
