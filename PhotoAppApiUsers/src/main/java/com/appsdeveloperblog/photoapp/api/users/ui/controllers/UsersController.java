9112efac8f25100576640ea1ed2341ef14e17a57
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-05-15 15:35:49 -0400

    Added the @PostAuthorize annotation example
    (cherry picked from commit 37a761a9f2e41043cbb99532ab356ef659d38e34)

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -57,7 +58,8 @@ public class UsersController {
 	}
 	
     @GetMapping(value="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    @PreAuthorize("principal == #userId")
+    //@PreAuthorize("principal == #userId")
+    @PostAuthorize("principal == returnObject.body.userId")
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
        
         UserDto userDto = usersService.getUserByUserId(userId); 
