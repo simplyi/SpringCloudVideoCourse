65d38d45a756430f0144941779832bca42c39fa5
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-01-18 12:11:56 -0500

    Annotated the getAlbums() method with @CircuitBreaker annotation. Added fallbackMethod.

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.photoapp.api.users.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,11 +9,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
 	@GetMapping("/users/{id}/albums")
+	@CircuitBreaker(name="albums-ws", fallbackMethod="getAlbumsFallback")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+	
+	default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exception) {
+		System.out.println("Param = " + id);
+		System.out.println("Exception took place: " + exception.getMessage());
+		return new ArrayList<>();
+		
+	}
 }
 
 
