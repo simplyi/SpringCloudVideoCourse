8bd0f8f19208a0a7caf21a763795aa4d222351f4
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-01-03 10:54:42 -0500

    Updated Eureka Server to be compatible with Spring Boot 3

--- PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -1,11 +1,13 @@
 package com.appsdeveloperblog.photoapp.discovery;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurity { //extends WebSecurityConfigurerAdapter {
 
