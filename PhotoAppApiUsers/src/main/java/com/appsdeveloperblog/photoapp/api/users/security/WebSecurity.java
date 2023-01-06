f5135b4af52ca1e7e6442716ecba315c6b219c34
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-01-06 12:04:50 -0500

    Added support for hasIpAddress()

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
@@ -46,10 +46,12 @@ public class WebSecurity {
         http
                 .cors().and()
                 .csrf().disable().authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users")
+                     .access(new WebExpressionAuthorizationManager(
+        				"hasIpAddress('"+environment.getProperty("gateway.ip")+"')"
+        				))
                 .requestMatchers(HttpMethod.POST, environment.getProperty("login.url.path")).permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
-                //.requestMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
                 .anyRequest().authenticated().and()
 
                 .addFilter(getAuthenticationFilter(authenticationManager))
