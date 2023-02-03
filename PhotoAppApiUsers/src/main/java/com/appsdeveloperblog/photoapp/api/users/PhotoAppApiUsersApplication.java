d162bdba6e1dfbf92781943ab474d69f27bddd0e
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-03 17:43:49 -0500

    Update to Spring Boot 3

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,15 +2,12 @@ package com.appsdeveloperblog.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
-
-import com.appsdeveloperblog.photoapp.api.users.shared.FeignErrorDecoder;
 
 import feign.Logger;
 
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,57 +1,66 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
+public class WebSecurity {
+
 	private Environment environment;
 	private UsersService usersService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
-	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder)
-	{
+	public WebSecurity(Environment environment, UsersService usersService,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.environment = environment;
 		this.usersService = usersService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+
+		// Get AuthenticationManager
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
 		http.csrf().disable();
-		http.authorizeRequests()
-		.antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
-		.antMatchers(HttpMethod.GET, "/actuator/health").hasIpAddress(environment.getProperty("gateway.ip"))
-		.antMatchers(HttpMethod.GET, "/actuator/circuitbreakerevents").hasIpAddress(environment.getProperty("gateway.ip"))
-		.and()
-		.addFilter(getAuthenticationFilter());
+		http.authorizeHttpRequests()
+
+				.requestMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+				.requestMatchers(HttpMethod.GET, "/actuator/circuitbreakerevents").permitAll().and()
+				.addFilter(getAuthenticationFilter(authenticationManager))
+				.authenticationManager(authenticationManager);
+
 		http.headers().frameOptions().disable();
+
+		return http.build();
 	}
-	
-	private AuthenticationFilter getAuthenticationFilter() throws Exception
-	{
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
-		//authenticationFilter.setAuthenticationManager(authenticationManager()); 
-		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
-		return authenticationFilter;
-	}
-	
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+
+	AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(usersService,environment, authenticationManager);
+        filter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+        return filter;
     }
+
+ 
 
 }
