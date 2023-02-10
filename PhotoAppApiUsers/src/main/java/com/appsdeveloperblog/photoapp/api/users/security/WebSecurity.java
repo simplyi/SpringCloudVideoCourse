bd072d94128c92e7fc611beb9135bcbc08bb5fe7
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-10 15:21:19 -0500

    Spring Boot 3 updates

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,53 +1,30 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
-	private Environment environment;
-	private UsersService usersService;
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+public class WebSecurity {
+    
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+  
+        http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+        .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+ 
+         http.headers().frameOptions().disable();
+        return http.build();
 
-	@Autowired
-	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder)
-	{
-		this.environment = environment;
-		this.usersService = usersService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
-		.and()
-		.addFilter(getAuthenticationFilter());
-		http.headers().frameOptions().disable();
-	}
-	
-	private AuthenticationFilter getAuthenticationFilter() throws Exception
-	{
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
-		//authenticationFilter.setAuthenticationManager(authenticationManager()); 
-		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
-		return authenticationFilter;
-	}
-	
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
     }
-
 }
