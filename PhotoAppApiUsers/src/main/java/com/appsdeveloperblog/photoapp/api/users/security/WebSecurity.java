ad5d9cfb1b4b0355cdd5c41608382172b8f89718
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-10 15:21:19 -0500

    Spring Boot 3 updates

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,29 +1,30 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
-	private Environment environment;
-	
-	@Autowired
-	public WebSecurity(Environment environment)
-	{
-		this.environment = environment;
-	}
+public class WebSecurity {
+    
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+  
+        http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+        .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+ 
+         http.headers().frameOptions().disable();
+        return http.build();
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
-		http.headers().frameOptions().disable();
-	}
-
+    }
 }
