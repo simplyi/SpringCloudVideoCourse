fe2a0e8273f107b4b04a4b498a2ca11cb7897f4c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-15 17:26:38 -0500

    Created AuthenticationFilter class. Implemented attemptAuthentication() method.
    (cherry picked from commit 30ce55b626ae3c3d43b37e1a214e3767a8d981e1)

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
@@ -2,79 +2,46 @@ package com.appsdeveloperblog.photoapp.api.users.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
-import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.LoginRequestModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-	private UsersService usersService;
-	private Environment environment;
-	
-	public AuthenticationFilter(UsersService usersService, 
-			Environment environment, 
-			AuthenticationManager authenticationManager) {
-		this.usersService = usersService;
-		this.environment = environment;
-		super.setAuthenticationManager(authenticationManager);
+	public AuthenticationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
 	}
-	
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest req,
-                                                HttpServletResponse res) throws AuthenticationException {
-        try {
-  
-            LoginRequestModel creds = new ObjectMapper()
-                    .readValue(req.getInputStream(), LoginRequestModel.class);
-            
-            return getAuthenticationManager().authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            creds.getEmail(),
-                            creds.getPassword(),
-                            new ArrayList<>())
-            );
-            
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    @Override
-    protected void successfulAuthentication(HttpServletRequest req,
-                                            HttpServletResponse res,
-                                            FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-    	String userName = ((User) auth.getPrincipal()).getUsername();
-    	UserDto userDetails = usersService.getUserDetailsByEmail(userName);
-    	
-        String token = Jwts.builder()
-                .setSubject(userDetails.getUserId())
-                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
-                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
-                .compact();
-        
-        res.addHeader("token", token);
-        res.addHeader("userId", userDetails.getUserId());
-    } 
-    
-	
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+			throws AuthenticationException {
+		try {
+
+			LoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), LoginRequestModel.class);
+
+			return getAuthenticationManager().authenticate(
+					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, 
+			HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
+	}
 }
