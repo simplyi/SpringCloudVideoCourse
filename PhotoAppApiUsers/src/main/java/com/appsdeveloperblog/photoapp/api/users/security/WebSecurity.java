6c516aa0080e4e7765b80cf7f1cc65cb3b8145b7
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-16 14:15:54 -0500

    Register AuthenticationFilter with HttpSecurity

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -3,6 +3,8 @@ package com.appsdeveloperblog.photoapp.api.users.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,12 +17,20 @@ public class WebSecurity {
     
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    	
+    	AuthenticationManagerBuilder authenticationManagerBuilder = 
+    			http.getSharedObject(AuthenticationManagerBuilder.class);
+    	
+    	AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+    	
         http.csrf().disable();
   
         http.authorizeHttpRequests()
         .requestMatchers(HttpMethod.POST, "/users").permitAll()
         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
         .and()
+        .addFilter(new AuthenticationFilter(authenticationManager))
+        .authenticationManager(authenticationManager)
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
  
          http.headers().frameOptions().disable();
