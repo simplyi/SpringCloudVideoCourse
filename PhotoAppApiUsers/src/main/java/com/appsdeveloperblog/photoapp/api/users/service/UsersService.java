fece14e74d9a82704f93ea6cecfc8bfc36b3bfc9
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-17 09:24:44 -0500

    Implemented loadUserByUsername()

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersService.java
@@ -1,7 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.users.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
 	UserDto createUser(UserDto userDetails);
 }
