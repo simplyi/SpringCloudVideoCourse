7a477553b3c10aaafb39a8748ef56bbf3c69ddd3
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-24 15:28:25 -0500

    Spring Boot 3 updates

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -3,58 +3,28 @@ package com.appsdeveloperblog.photoapp.api.users.data;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 
-import feign.FeignException;
-import feign.hystrix.FallbackFactory;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
-@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
+@FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
 	@GetMapping("/users/{id}/albums")
+	@Retry(name="albums-ws")
+	@CircuitBreaker(name="albums-ws", fallbackMethod="getAlbumsFallback")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
-}
-
-@Component
-class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
-
-	@Override
-	public AlbumsServiceClient create(Throwable cause) {
-		// TODO Auto-generated method stub
-		return new AlbumsServiceClientFallback(cause);
-	}
-
-}
-
-class AlbumsServiceClientFallback implements AlbumsServiceClient {
-
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	private final Throwable cause;
-
-	public AlbumsServiceClientFallback(Throwable cause) {
-		this.cause = cause;
-	}
-
-	@Override
-	public List<AlbumResponseModel> getAlbums(String id) {
-		// TODO Auto-generated method stub
-
-		if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
-			logger.error("404 error took place when getAlbums was called with userId: " + id + ". Error message: "
-					+ cause.getLocalizedMessage());
-		} else {
-			logger.error("Other error took place: " + cause.getLocalizedMessage());
-		}
-
+	
+	default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exception) {
+		System.out.println("Param = " + id);
+		System.out.println("Exception class=" + exception.getClass().getName());
+		System.out.println("Exception took place: " + exception.getMessage());
 		return new ArrayList<>();
+		
 	}
-
 }
