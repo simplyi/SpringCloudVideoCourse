62019feccd785d2160afb91d92e0e547b5c9b434
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-24 09:09:35 -0500

    Updated WebSecurity class

---
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.photoapp.discovery;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable()
+		    .authorizeHttpRequests()
+	     	.anyRequest().authenticated()
+	    	.and()
+	    	.httpBasic();
+
+		return http.build();
+	}
+}
