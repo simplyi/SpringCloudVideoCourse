01456ebc609d4ef27976fd42c7035ecc7763c184
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-24 09:18:42 -0500

    Updated Eureka to Spring Boot 3

--- PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -1,21 +1,24 @@
 package com.appsdeveloperblog.photoapp.discovery;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .authorizeRequests()
+public class WebSecurity {
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
-    }
 
+        return http.build();
+    }
 }
