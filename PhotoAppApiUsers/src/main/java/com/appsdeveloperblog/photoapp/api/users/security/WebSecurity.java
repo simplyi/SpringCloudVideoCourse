677dc131ef3eacd52afa0061dbad2ad7937571d3
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-03-02 18:10:19 -0500

    Enabled @EnableMethodSecurity annotation

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 
+@EnableMethodSecurity(prePostEnabled=true)
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
