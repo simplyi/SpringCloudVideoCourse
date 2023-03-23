a4359f58d484f638a69a68a5963ad7ebdb17bbb6
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-03-23 10:37:00 -0700

    Spring Boot 3 updates

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
@@ -1,13 +1,13 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Date;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,6 +24,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -58,23 +62,26 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         }
     }
     
-    @Override
-    protected void successfulAuthentication(HttpServletRequest req,
-                                            HttpServletResponse res,
-                                            FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-    	String userName = ((User) auth.getPrincipal()).getUsername();
-    	UserDto userDetails = usersService.getUserDetailsByEmail(userName);
-    	
-        String token = Jwts.builder()
-                .setSubject(userDetails.getUserId())
-                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
-                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
-                .compact();
-        
-        res.addHeader("token", token);
-        res.addHeader("userId", userDetails.getUserId());
-    } 
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
+		String userName = ((User) auth.getPrincipal()).getUsername();
+		UserDto userDetails = usersService.getUserDetailsByEmail(userName);
+		String tokenSecret = environment.getProperty("token.secret");
+		byte[] secretKeyBytes = Base64.getEncoder().encode(tokenSecret.getBytes());
+		SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+		Instant now = Instant.now();
+
+		String token = Jwts.builder().setSubject(userDetails.getUserId())
+				.setExpiration(
+						Date.from(now.plusMillis(Long.parseLong(environment.getProperty("token.expiration_time")))))
+				.setIssuedAt(Date.from(now)).signWith(secretKey, SignatureAlgorithm.HS512).compact();
+
+		res.addHeader("token", token);
+		res.addHeader("userId", userDetails.getUserId());
+	}
     
 	
 }
