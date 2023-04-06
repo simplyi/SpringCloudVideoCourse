76d58080877d3e04db2606bdfd5964dfaabc2f63
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-04-06 09:56:43 -0400

    Update to Spring Boot 3

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,53 +1,59 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
+public class WebSecurity {
+
 	private Environment environment;
 	private UsersService usersService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
-	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder)
-	{
+	public WebSecurity(Environment environment, UsersService usersService,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.environment = environment;
 		this.usersService = usersService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
-		.and()
-		.addFilter(getAuthenticationFilter());
-		http.headers().frameOptions().disable();
-	}
-	
-	private AuthenticationFilter getAuthenticationFilter() throws Exception
-	{
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
-		//authenticationFilter.setAuthenticationManager(authenticationManager()); 
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+
+		authenticationManagerBuilder.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		// Create AuthenticationFilter
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment,
+				authenticationManager);
 		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
-		return authenticationFilter;
+
+		http.csrf().disable();
+		http.authorizeHttpRequests().requestMatchers("/**")
+				.access(new WebExpressionAuthorizationManager(
+						"hasIpAddress('" + environment.getProperty("gateway.ip") + "')"))
+				.and().addFilter(authenticationFilter).authenticationManager(authenticationManager);
+
+		http.headers().frameOptions().disable();
+
+		return http.build();
 	}
-	
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
-    }
 
 }
