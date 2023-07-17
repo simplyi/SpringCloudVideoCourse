4b43a741ac2b34cc6036b18395b74c099a37fc66
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-07-17 12:10:42 -0400

    Added Collection<RoleEntity> roles to UserEntity class

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/UserEntity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/UserEntity.java
@@ -1,11 +1,17 @@
 package com.appsdeveloperblog.photoapp.api.users.data;
 
 import java.io.Serializable;
+import java.util.Collection;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +39,11 @@ public class UserEntity implements Serializable {
 	
 	@Column(nullable=false, unique=true)	
 	private String encryptedPassword;
+	
+	@ManyToMany(cascade=CascadeType.PERSIST, fetch=FetchType.EAGER)
+	@JoinTable(name="users_roles", joinColumns=@JoinColumn(name="users_id", referencedColumnName="id"), 
+			inverseJoinColumns=@JoinColumn(name="roles_id", referencedColumnName="id"))
+	Collection<RoleEntity> roles;
 	
 	
 	public long getId() {
