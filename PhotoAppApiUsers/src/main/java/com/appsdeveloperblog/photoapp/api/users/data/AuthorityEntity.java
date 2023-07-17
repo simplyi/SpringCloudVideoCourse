57cc1a5240c6695266b87b13b8db1183fd618197
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-07-17 18:59:18 -0400

    Created AuthorityEntity class

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AuthorityEntity.java
@@ -0,0 +1,55 @@
+package com.appsdeveloperblog.photoapp.api.users.data;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="authorities")
+public class AuthorityEntity implements Serializable {
+
+	private static final long serialVersionUID = 1618156374241833700L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false, length=20)
+	private String name;
+	
+	@ManyToMany(mappedBy="authorities")
+	private Collection<RoleEntity> roles;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collection<RoleEntity> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<RoleEntity> roles) {
+		this.roles = roles;
+	}
+	
+	
+
+}
