be1544f73d005ff772e1bfbb43d35f22ad792e72
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-07-18 12:19:03 -0400

    Created AuthorityRepository JPA Interface

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.photoapp.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorityRepository extends CrudRepository<AuthorityEntity, Long> {
+
+	AuthorityEntity findByName(String name);
+	
+}
