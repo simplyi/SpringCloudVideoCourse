839e12ffa831435447168749494bd9cde0b0718b
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2024-01-02 17:57:29 -0500

    Update to Spring Boot 3.2.1

--- PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.photoapp.discovery;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +14,9 @@ public class WebSecurity {
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable()
-		    .authorizeHttpRequests()
-	     	.anyRequest().authenticated()
-	    	.and()
-	    	.httpBasic();
+		http.csrf((csrf) -> csrf.disable());
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults());
 
 		return http.build();
 	}
